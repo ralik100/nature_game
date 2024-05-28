d5a37217787e5329907ffa313c2cc4c42b3556aa
SBagniewski <sbagniewski051@gmail.com>
2024-05-28 13:49:45 +0200

    1st commit

--- begin/Main.py
+++ begin/Main.py
@@ -2,6 +2,8 @@ from World import World # type: ignore
 from Position import Position # type: ignore
 from Organisms.Grass import Grass # type: ignore
 from Organisms.Sheep import Sheep # type: ignore
+from Organisms.Rys import Rys # type: ignore
+from Organisms.Antylopa import Antylopa # type: ignore
 import os
 
 
@@ -15,6 +17,12 @@ if __name__ == '__main__':
 	pyWorld.addOrganism(newOrg)
 
 	newOrg = Sheep(position=Position(xPosition=2, yPosition=2), world=pyWorld)
+	pyWorld.addOrganism(newOrg)
+
+	newOrg = Rys(position=Position(xPosition=5, yPosition=2), world=pyWorld)
+	pyWorld.addOrganism(newOrg)
+
+	newOrg = Antylopa(position=Position(xPosition=9, yPosition=3), world=pyWorld)
 	pyWorld.addOrganism(newOrg)
 
 	print(pyWorld)
---
+++ begin/Organisms/Antylopa.py
@@ -0,0 +1,20 @@
+from .Animal import Animal
+
+
+class Antylopa(Animal):
+
+	def __init__(self, antylopa=None, position=None, world=None):
+		super(Antylopa, self).__init__(antylopa, position, world)
+
+	def clone(self):
+		return Antylopa(self, None, None)
+
+	def initParams(self):
+		self.power = 4
+		self.initiative = 3
+		self.liveLength = 11
+		self.powerToReproduce = 5
+		self.sign = 'A'
+
+	def getNeighboringPosition(self):
+		return self.world.filterPositionsWithoutAnimals(self.world.getNeighboringPositions(self.position))
